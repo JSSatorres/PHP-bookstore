ae3ee675d2bf943bccf69c5d70063c2c99521ed1
jssatorres <juansataz.dev@gmail.com>
2022-11-18 08:53:30 +0100

    feat (App) inital styles

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="./assets/css/styles2.css">
+  <title>Start</title>
 </head>
 
 <body>
-  <h1>Please login or Signup</h1>
-  <a href="./view/login.php">Login</a> or
-  <a href="./view/register.php">Register</a>
+  <div class="container-main">
+    <h2>Please login or Sign up</h2>
+    <div class="container-wrapper-center login">
+      <a href="./view/login.php">Login</a>
+      <p> or </p>
+      <a href="./view/register.php">Sign up</a>
+    </div>
+  </div>
 </body>
 
 </html>
--- partials/header.php
+++ partials/header.php
@@ -5,13 +5,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/styles2.css">
   <title>Document</title>
 </head>
 
 <body>
-  <header>
-    <h1>A heading here</h1>
+  <header class="container-main">
+    <h1>Worker App</h1>
     <a href="../index.php">Home</a>
   </header>
 </body>
