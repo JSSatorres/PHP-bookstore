5a2815aa2c154bf3b2a07482f3bbfc2bd3f4e041
jssatorres <juansataz.dev@gmail.com>
2022-11-19 11:23:18 +0100

    Feat (app) new intial app point

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once './libs/app.php';
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./assets/css/styles2.css">
-  <title>Start</title>
-</head>
-
-<body>
-  <div class="container-main">
-    <h2>Please login or Sign up</h2>
-    <div class="container-wrapper-center login">
-      <a href="./view/login.php">Login</a>
-      <p> or </p>
-      <a href="./view/register.php">Sign up</a>
-    </div>
-  </div>
-</body>
-
-</html>
+$app = new App;
