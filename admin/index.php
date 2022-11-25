812f38307ccf2c95ea59280930b0bb223a49c9d0
jssatorres <juansataz.dev@gmail.com>
2022-11-25 12:40:38 +0100

    Feat (admin) new section

---
+++ admin/index.php
@@ -0,0 +1,26 @@
+<?php
+include "./template/header.php";
+?>
+<form action="index.php" method="post">
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="card col-4 mx-3 my-3 ">
+        <div class="form-group">
+          <h3 class="form-label my-4 text-center"> Login</h3>
+          <div class="form-floating mb-3 ">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-light btn-block my-3">Let me in.</button>
+      </div>
+    </div>
+  </div>
+</form>
+<?php
+include "./template/footer.php";
+?>
--- db/db.php
+++ db/db.php
@@ -1,7 +1,7 @@
 <?php
-
-class DB
+class Database
 {
+
   private $host;
   private $db;
   private $user;
@@ -10,30 +10,27 @@ class DB
 
   public function __construct()
   {
-    $this->host     = 'localhost';
-    $this->db       = 'login_manager';
-    $this->user     = 'root';
-    $this->password = "";
-    $this->charset  = 'utf8mb4';
+    $this->host = constant('localhost');
+    $this->db = constant('login-manager');
+    $this->user = constant('root');
+    $this->password = constant('');
+    $this->charset = constant('utf8mb4');
   }
 
   function connect()
   {
-
     try {
-
       $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
-     
+      $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_EMULATE_PREPARES   => false,
       ];
-      $pdo = new PDO($connection, $this->user, $this->password, $options);
 
+      $pdo = new PDO($connection, $this->user, $this->password, $options);
+      error_log('Conexión a BD exitosa');
       return $pdo;
     } catch (PDOException $e) {
-      print_r('Error connection: ' . $e->getMessage());
+      error_log('Error connection: ' . $e->getMessage());
     }
   }
 }
-
-
