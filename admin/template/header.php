58132c853f8097900be7ebde35dbec9857c6d6b6
jssatorres <juansataz.dev@gmail.com>
2022-11-27 19:51:32 +0100

    Fix admin header naming links

--- admin/template/header.php
+++ admin/template/header.php
@@ -9,28 +9,30 @@
   <link rel="stylesheet" href="../css/bootstrap.min.css">
 </head>
 
-<body>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarColor03">
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="./index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./products.php">Book</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./company.php">About</a>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <input class="form-control me-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
+<body></body>
+
+<?php $url = "http://" . $_SERVER['HTTP_HOST'] . "/Login/admin/product.php"; ?>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="./index.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./products.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $url ?>">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./company.php">Close</a>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
