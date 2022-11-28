2d04b9bad139049eb755fdb85d69f3a7ff7f8c03
jssatorres <juansataz.dev@gmail.com>
2022-11-28 19:16:10 +0100

    Fix (Db connection) repair connection to db

--- db/db.php
+++ db/db.php
@@ -1,36 +1,12 @@
 <?php
-class Database
-{
 
-  private $host;
-  private $db;
-  private $user;
-  private $password;
-  private $charset;
+$host = 'localhost';
+$db = 'login-manager';
+$user = 'root';
+$password = '';
 
-  public function __construct()
-  {
-    $this->host = constant('localhost');
-    $this->db = constant('login-manager');
-    $this->user = constant('root');
-    $this->password = constant('');
-    $this->charset = constant('utf8mb4');
-  }
-
-  function connect()
-  {
-    try {
-      $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
-      $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-      ];
-
-      $pdo = new PDO($connection, $this->user, $this->password, $options);
-      error_log('Conexión a BD exitosa');
-      return $pdo;
-    } catch (PDOException $e) {
-      error_log('Error connection: ' . $e->getMessage());
-    }
-  }
+try {
+  $conecttion = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+} catch (Exception $e) {
+  echo 'Connection Failed:' . $e->getMessage();
 }
