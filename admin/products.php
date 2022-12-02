fa617cc6e65e3a9847698ef1dc32f27d6e771496
jssatorres <juansataz.dev@gmail.com>
2022-12-02 12:18:18 +0100

    feat (admin) delete product

--- admin/products.php
+++ admin/products.php
@@ -1,10 +1,29 @@
 <?php
 include "./template/header.php";
+
+$id = (isset($_POST['id'])) ? $_POST['id'] : "";
+$action = (isset($_POST['action'])) ? $_POST['action'] : "";
+
+
 include "./db/db.php";
 
 $query = $conecttion->prepare("SELECT * FROM libros");
 $query->execute();
 $products = $query->fetchAll(PDO::FETCH_ASSOC);
+
+if ($action === "delete") {
+
+  $query = $conecttion->prepare("DELETE FROM libros WHERE id=:id");
+  $query->bindParam(':id', $id);
+  $query->execute();
+}
+
+if ($action === "edit") {
+
+  $query = $conecttion->prepare("SELECT * FROM libros WHERE id=:id");
+  $query->bindParam(':id', $id);
+  $query->execute();
+}
 ?>
 
 <div class="container">
@@ -29,9 +48,6 @@ $products = $query->fetchAll(PDO::FETCH_ASSOC);
         </div>
         <h5 class="card-subtitle text-muted mx-4 my-2"><strong> Properties:</strong></h5>
         <ul class="list-group list-group-flush">
-          <label class="form-check-label" for="flexCheckDefault">
-            <?php echo $product["store"] ?>
-          </label>
           <li class="list-group-item">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
               <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
@@ -51,20 +67,32 @@ $products = $query->fetchAll(PDO::FETCH_ASSOC);
             signed: <?php echo $product["signed"] ?>
           </li>
         </ul>
-        <div class="card-body">
-          <a href="#" class="card-link">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-              <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
-            </svg>
-            Delete
-          </a>
-          <a href="#" class="card-link">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-            </svg>
+        <div class="card-body container">
+          <form method="post" class="row">
+            <div class="col my-2">
+              <input type="hidden" name="id" value="<?php echo $product["id"] ?>" />
+              <button type="subtmit" class="btn btn-danger btn-sm" value="delete" name="action">
+                Delete
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                </svg>
+              </button>
+              <input type="hidden" name="id" value="<?php echo $product["id"] ?>" />
+            </div>
+            <div class="col my-2">
+              <button type="subtmit" class="btn btn-danger btn-sm" value="edit" name="action">
+                Edit
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                </svg>
+              </button>
+            </div>
+          </form>
+          <!-- <a href="#" class="card-link">
+ 
             Edit
-          </a>
+          </a> -->
         </div>
       </div>
     <?php
---
+++ template/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Employ Management</title>
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+
+<body></body>
+
+<?php $url = "http://" . $_SERVER['HTTP_HOST'] . "/Login/admin/add-product.php"; ?>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="./index.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./products.php">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $url ?>">Add Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./company.php">Close</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
